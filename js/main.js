b94a1b4474df1e83f8c190d653aa6786c223b2fb
William Roger <williamroger@MacBook-Pro-de-William-Roger.local>
2016-09-26 20:30:35 -0300

    Adicionando Animações com CSS e JS

--- js/main.js
+++ js/main.js
@@ -10,3 +10,29 @@ $(".rslides_portifolio").responsiveSlides({
   timeout: 7000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
 });
+
+Visibility.onVisible(function(){
+  setTimeout(function() {
+    $(".introducao h1").addClass("animated fadeInDown");
+  }, 400);
+  setTimeout(function() {
+    $(".introducao blockquote").addClass("animated fadeInDown");
+  }, 800);
+  setTimeout(function() {
+    $(".introducao .btn").addClass("animated fadeInDown");
+  }, 1200);
+  setTimeout(function() {
+    $(".animar").addClass("animated fadeInDown");
+  }, 1600);
+
+
+  setTimeout(function() {
+    $(".introducao_interna h1").addClass("animated fadeInDown");
+  }, 400);
+  setTimeout(function() {
+    $(".introducao_interna p").addClass("animated fadeInDown");
+  }, 800);
+  setTimeout(function() {
+    $(".animar-interno").addClass("animated fadeInDown");
+  }, 1200);
+});
